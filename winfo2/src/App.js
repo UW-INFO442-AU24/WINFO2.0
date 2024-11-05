f31c0d44f31a78f64a691bcaf8008534bdc486b1
g3xo <102757915+g3xo@users.noreply.github.com>
2024-11-05 11:22:27 -0800

    Add character building page component

--- winfo2/src/App.js
+++ winfo2/src/App.js
@@ -1,15 +1,16 @@
+\import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AccessoriesFilter from './components/AccessoriesFilter/AccessoriesFilter.js';
 import Book from './components/Book/Book.js';
 import BookFilter from './components/BookFilter/BookFilter.js';
-import CharacterBuilding from './components/CharacterBuilding/CharacterBuilding.js';
+import CharacterBuilding from './components/CharacterBuilding/CharacterBuilding.js'; // Check this path
 import Home from './components/Home/Home.js';
-import Inventory from './components/Invnetory/Inventory.js';
+import Inventory from './components/Inventory/Inventory.js';
 import MarketPlace from './components/MarketPlace/MarketPlace.js';
 import NavBar from './components/NavBar/NavBar.js';
 import Profile from './components/Profile/Profile.js';
 import ProgressBar from './components/ProgressBar/ProgressBar.js';
 import Quiz from './components/Quiz/Quiz.js';
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 export default function App(props) {
     return (
@@ -19,11 +20,10 @@ export default function App(props) {
             <Route path="/AccessoriesFilter" element={<AccessoriesFilter />} />
             <Route path="/Book" element={<Book/>} />
             <Route path="/BookFilter" element={<BookFilter/>} />
-            <Route path="/CharacterBuilding" element={<CharacterBuilding/>} />
+            <Route path="/CharacterBuilding" element={<CharacterBuilding/>} /> {/* Route for Character Building */}
             <Route path="/Home" element={<Home/>} />
             <Route path="/Inventory" element={<Inventory/>} />
             <Route path="/MarketPlace" element={<MarketPlace/>} />
-            <Route path="/NavBar" element={<NavBar/>} />
             <Route path="/Profile" element={<Profile/>} />
             <Route path="/ProgressBar" element={<ProgressBar/>} />
             <Route path="/Quiz" element={<Quiz/>} />
@@ -32,4 +32,3 @@ export default function App(props) {
       </div>
     );
   }
-  
